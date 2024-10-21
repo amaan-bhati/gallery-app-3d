759f7a2f441a7d7fab6986342f9389507745b6e7
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-10-21 15:08:03 +0530

    init: bolier-place-with-tailwindcss

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return(
+    <main className="p-10">
+      <h1 className="text-3xl font-bold underline underline-offset-2 decoration-[1px]">
+        3D animation effect using Aceternity UI
+      </h1>
+    </main>
+  );
+}
